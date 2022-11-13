a1afcee89e532eca32dd73c5cbabe98aa75dd587
raymo108 <60206015+raymo108@users.noreply.github.com>
2022-11-13 08:39:36 -0500

    Update nfl_twitter_bot.py

--- nfl_twitter_bot.py
+++ nfl_twitter_bot.py
@@ -1,40 +1,36 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[ ]:
-
-
 import tweepy
 from GoogleNews import GoogleNews
 from random import randint
+import time
 
-client = tweepy.Client(consumer_key='YOUR_CONSUMER_KEY_HERE',
-                       consumer_secret='YOUR_CONSUMER_SECRET_HERE',
+while(True):
+
+        client = tweepy.Client(consumer_key='YOUR_CONSUMER_KEY_HERE',
+                       consumer_secret='YOUR_SECRET_KEY_HERE',
                        bearer_token='YOUR_BEARER_TOKEN_HERE',
                        access_token='YOUR_ACCESS_TOKEN_HERE',
                        access_token_secret='YOUR_ACCESS_TOKEN_SECRET_HERE')
 
-gn = GoogleNews(period='12h')
-gn.get_news('nfl')
-result = gn.result()
+        gn = GoogleNews(period='12h')
+        gn.get_news('nfl')
+        result = gn.result()
 
-for items in result:
-    title = items['title']
-    link = items['link']
+        for items in result:
+                title = items['title']
+                link = items['link']
     
-    element_info = {
-        "Title": title,
-        "Link": link
-    }
-# GoogleNews pulls articles as "items"
+                element_info = {
+                        "Title": title,
+                        "Link": link
+                }
+
 #Find random article in last 24 hours using randint between index 0 and the last index
-article=[items][randint( 0, len([items])-1 ) ]
+        article=[items][randint( 0, len([items])-1 ) ]
 
 #construct the tweet text
-tweet_text = f"In NFL News: {title}. See full article: {link}. #nfl #nflnews #football #sports"
+        tweet_text = f"In NFL News: {title}. See full article: {link}. #nfl #nflnews #football #sports"
 
-#Fire off the tweet!
-
-response = client.create_tweet(text=tweet_text)
-print(response)
-
+#send tweet from account
+        response = client.create_tweet(text=tweet_text)
+        
+        time.sleep(7200)
